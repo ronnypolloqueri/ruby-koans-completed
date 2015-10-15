708923925e351f853cd80744e08c6b7db6262d49
Ronny Polloqueri <ronny.polloqueri@gmail.com>
2015-10-15 17:20:48 -0500

    Ruby koans Completed

---
+++ README.md
@@ -0,0 +1,4 @@
+# ruby-koans-completed
+[Visita ruby-koans](http://rubykoans.com/)
+
+**Nota:** Agrego libnotify, (0.9.1) para usar notificaciones durante las pruebas
---
+++ triangle.rb
@@ -0,0 +1,41 @@
+# Triangle Project Code.
+
+# Triangle analyzes the lengths of the sides of a triangle
+# (represented by a, b and c) and returns the type of triangle.
+#
+# It returns:
+#   :equilateral  if all sides are equal
+#   :isosceles    if exactly 2 sides are equal
+#   :scalene      if no sides are equal
+#
+# The tests for this method can be found in
+#   about_triangle_project.rb
+# and
+#   about_triangle_project_2.rb
+#
+def verify_true_triangle_isosceles(side_equal, other_side)
+  # /\     /       \
+  # --     ---------
+  # 2,1,1 ; 10, 1,1  (false triangle)
+  raise TriangleError if side_equal * 2 <= other_side
+end
+
+def triangle(a, b, c)
+  # WRITE THIS CODE
+  raise TriangleError if a <= 0 or b <= 0 or c <= 0
+  if a == b and b == c
+    return :equilateral 
+  elsif a == b or b == c or a == c
+    verify_true_triangle_isosceles(a,c) if a==b
+    verify_true_triangle_isosceles(b,a) if b==c
+    verify_true_triangle_isosceles(a,b) if a==c
+    return :isosceles 
+  else
+    return :scalene
+  end
+
+end
+
+# Error class used in part 2.  No need to change this code.
+class TriangleError < StandardError
+end
